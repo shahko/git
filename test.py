094ace0c21a23c423db25283ce4ac3e63bc3fff1
shahko <a01089772905@gmail.com>
2024-06-03 23:05:11 +0900

    a

--- test.py
+++ test.py
@@ -17,24 +17,24 @@ def login(username, password):
         st.session_state.login_status = True
         st.session_state.username = username
     else:
-        st.error("Invalid username or password")
+        st.error("사용자 이름 또는 비밀번호가 잘못되었습니다")
 
 def logout():
     st.session_state.login_status = False
     st.session_state.username = ''
 
 def main():
-    st.title("Login Page")
+    st.title("로그인 페이지")
 
     if st.session_state.login_status:
-        st.write(f"Welcome, {st.session_state.username}!")
-        if st.button("Logout"):
+        st.write(f"{st.session_state.username}님, 환영합니다!")
+        if st.button("로그아웃"):
             logout()
     else:
-        st.subheader("Please log in")
-        username = st.text_input("Username")
-        password = st.text_input("Password", type='password')
-        if st.button("Login"):
+        st.subheader("로그인 해주세요")
+        username = st.text_input("사용자 이름")
+        password = st.text_input("비밀번호", type='password')
+        if st.button("로그인"):
             login(username, password)
 
 if __name__ == "__main__":
